cd4b4a6be401d9cdec7270c5e5e9a6075dbb85d5
Bill Jones <misterunix@gmail.com>
2025-05-11 19:20:44 -0500

    Add documentation generation for main, common, and pdf modules

--- cmd/app/mynotebook/build.sh
+++ cmd/app/mynotebook/build.sh
@@ -4,3 +4,9 @@ GOOS=linux GOARCH=amd64 go build -gcflags="all=-N -l" -o linux-amd64/mynotebook-
 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o linux-amd64/mynotebook-amd64-min
 GOOS=windows GOARCH=amd64 go build -gcflags="all=-N -l" -o windows-amd64/mynotebook-debug-amd64.exe
 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o windows-amd64/mynotebook-amd64.exe
+
+
+gomarkdoc -u -o ../../../docs/main.md main.go
+gomarkdoc -u -o ../../../docs/common.md ../../../internal/common
+gomarkdoc -u -o ../../../docs/pdf.md ../../../internal/pdf 
+
